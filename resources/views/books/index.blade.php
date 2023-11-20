b7773b930b5d4ef3054a7f3d34e2ec86661eb96a
Chloe Dwyer <chloedwyer@MacBook-Pro.local>
2023-11-20 12:02:04 +0000

    Added many to many relationship and seeding

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Publishers') }}
+            {{ __('Books') }}
         </h2>
     </x-slot>
 
@@ -12,13 +12,19 @@
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" class="px-6 py-3">
-                                Name
+                                Title
                             </th>
                             <th scope="col" class="px-6 py-3">
-                                Email
+                                Description
                             </th>
                             <th scope="col" class="px-6 py-3">
-                                Phone
+                                ISBN
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Publisher
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Created At
                             </th>
                             <th scope="col" class="px-6 py-3">
                                 Action
@@ -27,20 +33,26 @@
                     </thead>
                     <tbody>
             
-                        @forelse($publishers as $publisher)
+                        @forelse($books as $book)
                         <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{ $publisher->name }}
+                                {{ $book->title }}
                             </th>
                             <td class="px-6 py-4">
-                                {{ $publisher->email }}
+                                {{ $book->description }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ $publisher->created_at }}
+                                {{ $book->isbn }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $book->publisher->name }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $book->created_at }}
                             </td>
                             {{-- Button --}}
                             <td class="px-6 py-4">
-                                <a href="{{ route('publishers.show', $publisher->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                                <a href="{{ route('books.show', $book->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
                             </td>
                         </tr>
                         @empty
